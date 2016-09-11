e052ae6f7f0e84a73fd94f9583d62099d09fc13b
kian-ja <seyed.jalaleddini@mail.mcgill.ca>
2016-09-10 18:42:03 -0700

    function uniform_LPF clean up

--- simulations/uniform_LPF.m
+++ simulations/uniform_LPF.m
@@ -5,9 +5,8 @@ uniformSignal = rand(numSamples,1) - 0.5;
 uniformSignal = uniformSignal * (maxInput - minInput);
 uniformSignal = uniformSignal + (maxInput + minInput)/2;
 uniformLPF_Signal = uniformSignal;
-%uniformLPF_Signal = sort(uniformLPF_Signal);
-[uniformLPF_SignalSorted,sortIndexUniform] = sort(uniformLPF_Signal);
-[inputGaussianSorted,sortIndexGaussian] = sort(inputGaussian);
+[~,sortIndexUniform] = sort(uniformLPF_Signal);
+[~,sortIndexGaussian] = sort(inputGaussian);
 uniformLPF_Signal(sortIndexGaussian) = uniformLPF_Signal(sortIndexUniform);
 uniformLPF_Signal = nldat(uniformLPF_Signal- mean (uniformLPF_Signal),'domainIncr',0.001);
 plot(spect(uniformLPF_Signal))
