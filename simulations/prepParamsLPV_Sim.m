70b9df489c7d0d4aad89d71aa0b100fc1d461846
kian-ja <seyed.jalaleddini@mail.mcgill.ca>
2016-09-17 11:39:51 -0700

    plot figures, changing the reflex gain must run simulations again

--- simulations/prepParamsLPV_Sim.m
+++ simulations/prepParamsLPV_Sim.m
@@ -8,6 +8,7 @@ h = irfModel.dataSet;
 set_param('stiffnessLPVModel/irfCoeff','Value',['[',num2str(h'),']']);
 positionLevels = [-0.48 -0.4 -0.32 -0.24 -0.16 -0.08 0.0 0.08 0.16 0.24];%from Mirbagheri et al 2000
 Gr = [0.0625,0.0938,0.1042,0.1094,0.2813,0.5000,0.6250,0.7000,0.8000,0.8100];%from Mirbagheri et al 2000
+Gr = Gr + 0.19;
 K = [0.38 0.36 0.42 0.48 0.52 0.55 0.6 0.7 0.8 1];%from Mirbagheri et al 2000
 %omega = [0.5 0.625 0.69 0.625 0.52 0.38 0.379 0.35 0.56 0.52];%from Mirbagheri et al 2000
 %zeta = [0.5 0.25 0.36 0.33 0.36 0.33 0.37 0.39 0.58 0.55];%from Mirbagheri et al 2000
@@ -18,7 +19,7 @@ set_param('stiffnessLPVModel/elasticSubjectNormalizeGain','Gain',num2str(50));%f
 polyCoeffGr = polyfit(positionLevels,Gr,5);
 set_param('stiffnessLPVModel/reflexGainPolynomialCoeff','Value',['[',num2str(polyCoeffGr),']']);
 set_param('stiffnessLPVModel/reflexGainSubjectNormalizeGain','Gain',num2str(7.3));%from Mirbagheri et al 2000, subject HB
-set_param('stiffnessLPVModel/reflexGainSubjectNormalizeGain','Gain',num2str(30));%from Mirbagheri et al 2000, subject HB
+set_param('stiffnessLPVModel/reflexGainSubjectNormalizeGain','Gain',num2str(40));%from Mirbagheri et al 2000, subject HB
 %polyCoeffOmega = polyfit(positionLevels,omega,5);
 %set_param('reflexStiffnessLPVModel/reflexOmegaPolynomialCoeff','Value',['[',num2str(polyCoeffOmega),']']);
 set_param('stiffnessLPVModel/reflexOmegaSubjectNormalizeGain','Gain',num2str(50));%from Mirbagheri et al 2000, subject HB
