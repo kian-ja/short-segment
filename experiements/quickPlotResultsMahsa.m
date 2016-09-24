526b22fa6820f9d536c72cafc25188adbfad36f7
Kian <seyed.jalaleddini@mail.mcgill.ca>
2016-09-24 10:49:29 -0700

    plotting SDSS vs SS-SDSS experiment

--- experiements/quickPlotResultsMahsa.m
+++ experiements/quickPlotResultsMahsa.m
@@ -5,20 +5,24 @@ numLevelsLen = length(sysID);
 K = cell(numLevelsLen,1);
 Gr = cell(numLevelsLen,1);
 vafTot = cell(numLevelsLen,1);
+vafTotSDSS = cell(numLevelsLen,1);
 vafIntrinsic = cell(numLevelsLen,1);
 vafReflex = cell(numLevelsLen,1);
 x = -2 :0.001:2;
 x = nldat(x','domainIncr',0.001);
 for i = 1 : numLevelsLen
     sysIDTemp = sysID{i};
+    sysID_SDSSTemp = sysID_SDSS{i};
     KTemp = zeros(size(sysID{i},1),size(sysIDTemp,2));
     vafTotTemp = zeros(size(sysID{i},1),size(sysIDTemp,2));
+    vafTot_SDSS_Temp = zeros(size(sysID{i},1),size(sysIDTemp,2));
     vafIntrinsicTemp = zeros(size(sysID{i},1),size(sysIDTemp,2));
     vafReflexTemp = zeros(size(sysID{i},1),size(sysIDTemp,2));
     GrTemp = zeros(size(sysID{i},1),size(sysIDTemp,2));
     for j = 1 : size(sysIDTemp,1)%levels
         for k = 1 : size(sysIDTemp,2)%MC iteration
             systemTemp = sysIDTemp{j,k};
+            systemTemp_SDSS = sysID_SDSSTemp{j,k};
             intrinsicTemp = systemTemp{1};
             reflexTemp = systemTemp{2};
             reflexNLTemp = reflexTemp{1};
@@ -30,15 +34,19 @@ for i = 1 : numLevelsLen
             end
             GrTemp(j,k) = slope;
             vafsTemp = systemTemp{3};
+            
             KTemp(j,k)= max(-0.01*sum(intrinsicTemp.dataSet),0);
             vafTotTemp(j,k)= vafsTemp(1);
             vafIntrinsicTemp(j,k)= vafsTemp(2);
             vafReflexTemp(j,k)= vafsTemp(3);
+            vafsTemp_SDSS = systemTemp_SDSS{3};
+            vafTot_SDSS_Temp(j,k)= vafsTemp_SDSS(1);
         end
     end
     K{i} = KTemp;
     Gr{i} = GrTemp;
     vafTot{i} = vafTotTemp;
+    vafTotSDSS{i} = vafTot_SDSS_Temp;
     vafIntrinsic{i} = vafIntrinsicTemp;
     vafReflex{i} = vafReflexTemp;
 end
@@ -81,3 +89,22 @@ subplot(1,2,2)
 xlabel('Torque (Nm)')
 ylabel('Reflex Gain (Nms/rad)')
 title('Reflex Gain')
+%%
+figure
+hold on
+for i = 1 : numLevelsLen
+    vafTot_SS_SDSSThisLevel = vafTot{i};
+    vafTot_SDSSThisLevel = vafTotSDSS{i};
+    vafTot_SS_SDSSThisLevel = vafTot_SS_SDSSThisLevel(:);
+    vafTot_SDSSThisLevel = vafTot_SDSSThisLevel(:);
+    vafTot_SS_SDSSThisLevelMean = mean(vafTot_SS_SDSSThisLevel);
+    vafTot_SS_SDSSThisLevel5 = prctile(vafTot_SS_SDSSThisLevel,5);
+    vafTot_SS_SDSSThisLevel95 = prctile(vafTot_SS_SDSSThisLevel,95);
+    
+    vafTot_SDSSThisLevelMean = mean(vafTot_SDSSThisLevel);
+    vafTot_SDSSThisLevel5 = prctile(vafTot_SDSSThisLevel,5);
+    vafTot_SDSSThisLevel95 = prctile(vafTot_SDSSThisLevel,95);
+    bar(numLevels(i)-0.15,vafTot_SDSSThisLevelMean,0.25)
+    bar(numLevels(i)+0.15,vafTot_SS_SDSSThisLevelMean,0.25)
+
+end
